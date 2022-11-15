74836626225b413d977b09320420bea097a2c9de
pierre01 <pierre@delange.design>
2022-11-14 17:45:16 -0800

    User Interface and triggers

--- 21stMortgageInterviewApplication/MainWindow.xaml.cs
+++ 21stMortgageInterviewApplication/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using _21stMortgageInterviewApplication.ViewModels;
 
 namespace _21stMortgageInterviewApplication
 {
@@ -23,6 +11,7 @@ namespace _21stMortgageInterviewApplication
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
     }
 }
